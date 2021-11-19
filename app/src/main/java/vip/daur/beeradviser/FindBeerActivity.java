238e4cc61a0d3ce22587f0752e38d8ec10aa289c
daur-vip <mail@daur.vip>
2021-11-19 17:47:20 +0600

    added onclick

--- app/src/main/java/vip/daur/beeradviser/FindBeerActivity.java
+++ app/src/main/java/vip/daur/beeradviser/FindBeerActivity.java
@@ -2,11 +2,16 @@ package vip.daur.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends Activity {
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+
     }
 }
